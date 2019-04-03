e1a203019f09a8d7c099adc3b8c057b59e4fc55a
Iris The Fox <iris-miz@yandex.ru>
2019-04-03 18:31:03 +0300

    cat vomits

--- js/script.js
+++ js/script.js
@@ -18,30 +18,34 @@ document.addEventListener("DOMContentLoaded",
       .addEventListener("mousemove",
         function (event) {
           if (event.shiftKey === true) {
-          	            console.log("x: " + event.clientX);
-            console.log("y: " + event.clientY);
-          if (event.clientX > 970 && event.clientX < 1150 && event.clientY > 180 && event.clientY < 400) {
             console.log("x: " + event.clientX);
             console.log("y: " + event.clientY);
-            console.log("Dont touch me!")
-            document
+          if ( event.clientX < 1070 && event.clientY < 350 ) {
+                        document
+            .querySelector("#tummy")
+            .textContent = "Purr! Purr! Purr!";         
+
+          }
+          else if ((event.clientX > 1070 && event.clientX < 1150 && event.clientY < 400) || ( event.clientX < 1070 && event.clientY > 350 && event.clientY < 415) ) {
+                        document
             .querySelector("#tummy")
             .textContent = "Dont touch me!";
-          }
-          else if (event.clientX > 1150) {
-                       document
-            .querySelector("#tummy")
-            .innerHTML = "<div class='angry'>Dont touch me! <br> I'll kill you <br> and everyone <br> you love!!!</div>";
+
+
           }
           else {
-            document
+                       document
             .querySelector("#tummy")
-            .textContent = "Purr! Purr! Purr!";           
+            .innerHTML = "<div class='angry'>Dont touch me! <br> I'll kill you <br> and everyone <br> you love!!!</div>";        
           }
-        	}
+          }
 
         }
       );
 
-  }
-);
+  })
+
+
+function myFunction() {
+  alert('Cat vomits');
+}
